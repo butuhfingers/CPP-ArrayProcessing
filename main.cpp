505c0dead439fbd0c8d666007e75541e2bd6650b
butuhfingers <crewderek@yahoo.com>
2017-10-01 22:06:30 -0600

    Working array creation and traversal

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
+    //Create the array
+    int integerArray[2000];
+
+    //Go through the array and create a random number for each
+    int i = -1;
+    while(++i < 2000){
+        integerArray[i] = rand() % 2000;
+        std::cout << "Intger: " << integerArray[i] << std::endl;
+    }
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
