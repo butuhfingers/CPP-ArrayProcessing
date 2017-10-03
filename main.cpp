0058825237d1db93e36aba7f2725b1ec443146de
derek <derek.crew@rocky.edu>
2017-10-03 15:49:56 -0600

    Finalized project

--- main.cpp
+++ main.cpp
@@ -8,9 +8,18 @@ int main() {
     int i = -1;
     while(++i < 2000){
         integerArray[i] = rand() % 2000;
-        std::cout << "Intger: " << integerArray[i] << std::endl;
+        std::cout << "Intger[" << i << "]: " << integerArray[i] << std::endl;
     }
 
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "------------Start the pointer arithmetic--------------" << std::endl;
+
+    //Create a pointer
+    int* pointer = integerArray;
+
+    //Go through the "pointer array" as a pointer
+    for(i = 0; i < 2000; pointer++,i++){
+        std::cout << "Pointer[" << i << "]: " << *pointer << std::endl;
+    }
+
     return 0;
 }
